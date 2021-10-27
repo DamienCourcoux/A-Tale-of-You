09ba0bd8fa398dfc7fe07af88d92c8a02db5b3bc
DamienCourcoux <damsmailspro@gmail.com>
2021-10-27 12:24:32 +0200

    Ajout du Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,35 @@
-import Signup from "src/components/Signup";
-import Signin from "src/components/Signin";
+import Signup from 'src/components/Signup';
+import Signin from 'src/components/Signin';
+
+import Logo from 'src/assets/logo.gif';
+import { FaBook, FaScroll } from 'react-icons/fa';
+
+import './header.scss';
 
 const Header = () => (
-  <nav className="Header">
-    <div>Index</div>
-    <div>Règles du jeu</div>
-    <div>Jouer</div>
-    <Signup />
-    <Signin />
-  </nav>
-)
+  <header className="header">
+    <nav className="header__nav">
+      <ul className="header__nav--games">
+        <li>
+          <a href="/" title="A Tale of You"><img src={Logo} alt="Logo du site" /></a>
+        </li>
+        <li>
+          <a href="/" title="Jouer"><FaBook /> Jouer</a>
+        </li>
+        <li>
+          <a href="/" title="Règles du jeu"><FaScroll /> Règles du jeu</a>
+        </li>
+      </ul>
+      <ul className="header__nav--settings">
+        <li>
+          <Signup />
+        </li>
+        <li>
+          <Signin />
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
