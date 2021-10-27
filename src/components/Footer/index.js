c502e32eaf18bd3faa187fac99ce81bf76bf7b58
DamienCourcoux <damsmailspro@gmail.com>
2021-10-27 13:04:24 +0200

    ajout footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,20 @@
+import Logo from 'src/assets/logo.gif';
+import { FaGithub, FaTwitter, FaFacebook } from 'react-icons/fa';
+
+import './footer.scss';
+
 const Footer = () => (
-    <div className="Footer">
-      Footer
+  <footer className="footer">
+    <a className="footer__logo" href="/"><img src={Logo} alt="Logo du site" /></a>
+    <div className="footer__links">
+      <ul className="footer__links--reseaux">
+        <li><a href="/" target="_blank" rel="noopener noreferrer"><FaGithub /> </a></li>
+        <li><a href="/" target="_blank" rel="noopener noreferrer"><FaTwitter /> </a></li>
+        <li><a href="/" target="_blank" rel="noopener noreferrer"><FaFacebook /> </a></li>
+      </ul>
+      <a className="footer__links--aboutUs" href="/">A propos de nous</a>
+      <p className="footer__links--copyright">© 2021 <span>A Tale of You</span>. Tous droits réservés.</p>
     </div>
-)
+  </footer>
+);
 export default Footer;
