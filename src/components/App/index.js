f24d52613307f785e1f3b713115c2d89542cebd4
Maxence-TASSERY <maxence.tassery@gmail.com>
2021-10-27 12:36:59 +0200

    Création du composant Rules

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 // == Import
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
+import Rules from 'src/components/Rules';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Rules />
     <Footer />
   </div>
 );
---
+++ src/components/Rules/index.js
@@ -0,0 +1,22 @@
+import './styles.scss';
+
+// == Composant
+const Rules = () => (
+  <div className="rules">
+    <p>Règles du jeu</p>
+    
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse adipisci cumque doloremque pariatur atque harum mollitia veniam saepe magni optio incidunt, quisquam deserunt doloribus omnis, voluptates porro nisi enim odit?
+    Rerum placeat asperiores sapiente minima, a qui totam eos, tempore deserunt iure, dolores iusto? Incidunt necessitatibus placeat laborum nemo minus, dolores consequuntur. Fuga modi error corrupti pariatur nobis nostrum labore?
+    Voluptatum perferendis suscipit non soluta doloremque amet accusamus distinctio necessitatibus ab a quia, earum provident, in repudiandae quasi natus nobis dolore velit porro. Natus iusto, aperiam ad aliquid asperiores eius.
+    Temporibus, molestias nihil. Quod nostrum doloremque cum nesciunt quo perferendis natus. Iste quia voluptatibus maxime aliquam sint similique esse magni quaerat velit cupiditate, sequi non deserunt corporis reiciendis vero hic!
+    Deserunt ullam nemo dolore modi adipisci possimus rerum perferendis, optio sed molestiae dignissimos rem cumque consequatur deleniti labore enim pariatur nihil nobis vero sapiente soluta. Magnam quibusdam enim deleniti possimus?
+    Ut doloribus voluptates eum laboriosam iusto illum distinctio nesciunt quidem magnam delectus commodi voluptas molestias aspernatur corporis, aliquam et quasi quod ullam temporibus rem sapiente quis in? Officiis, optio recusandae?
+    Deleniti qui quas odit repellat enim eius explicabo nobis minus aliquam omnis fugiat, beatae esse at minima. Harum corporis veritatis, provident libero adipisci modi fuga. Quo voluptatibus ullam nihil dignissimos?
+    Aspernatur excepturi sit reiciendis quisquam at, laborum ratione, vitae libero cupiditate amet officiis, ad voluptate. Id, sit consectetur mollitia voluptas commodi, reprehenderit adipisci, omnis beatae delectus tempore asperiores dolore. Deleniti!
+    Ratione repellendus, ut expedita placeat assumenda qui non veritatis quisquam ex, ad deserunt aperiam in praesentium minus rem sit aspernatur minima maxime? Dolores quis soluta consectetur enim eos perferendis quas.
+    Molestiae necessitatibus nostrum commodi, voluptas quisquam at vitae saepe optio impedit sit fugit architecto deserunt omnis ipsum delectus eum totam dolorum ex a suscipit distinctio aperiam beatae repudiandae? Nostrum, expedita?
+  </div>
+);
+
+// == Export
+export default Rules
