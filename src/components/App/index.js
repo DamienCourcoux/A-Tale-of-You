bffaae9d8099afe5d91c03fabdeeec95f0803c5e
DamienCourcoux <damsmailspro@gmail.com>
2021-10-27 16:01:58 +0200

    ajout index

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 // == Import
 import Header from 'src/components/Header';
+import Index from 'src/components/Index';
 import Footer from 'src/components/Footer';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Index />
     <Footer />
   </div>
 );
