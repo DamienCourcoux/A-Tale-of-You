54201fb649b13976682b518518ddb9e1770e8b86
Maxence-TASSERY <maxence.tassery@gmail.com>
2021-10-29 14:44:37 +0200

    Début création Signup

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,7 +1,27 @@
-const Signup = () => (
-  <div className="Signup">
-    Signup
-  </div>
-);
+import { useSelector, useDispatch } from 'react-redux';
+
+import './styles.scss';
+
+const Signup = () => {
+  const dispatch = useDispatch();
+
+  const {
+    pseudo,
+    email,
+    password,
+    confirmPassword,
+  } = useSelector((state) => ({
+    pseudo: state.user.pseudo,
+    email: state.user.email,
+    password: state.user.password,
+    confirmPassword: state.user.confirmPassword,
+  }));
+
+  return (
+    <div className="Signup">
+      Signup
+    </div>
+  );
+};
 
 export default Signup;
