cc8dec886ed84468a8f0ca093ef98159a7babcc0
guillaume806 <guillaumebodart@outlook.fr>
2021-11-02 18:54:31 +0100

    regles-jeu-scss

--- src/components/Rules/index.js
+++ src/components/Rules/index.js
@@ -1,13 +1,14 @@
 import './styles.scss';
+import { FaImages } from 'react-icons/fa';
 
 // == Composant
 const Rules = () => (
   <section className="rules">
     <div className="rules__border shadow">
       <div className="rules__pages shadow">
-        <div className="rules__page shadow">  
+        <div className="rules__page shadow">
           <h1>Règles du jeu</h1>
-
+          <div className="hr" />
           {/* <div className="rules__h2">Introduction</div>
 
           <div className="rules__text">
@@ -26,23 +27,27 @@ const Rules = () => (
 
           <p>
             Les entités possèdent quatre caractéristiques fixes au cours de la partie :
-            <ul>
-              <li>La force (FOR en jeu)</li>
-              <li>La dextérité (DEX en jeu)</li>
-              <li>L'intelligence (INT en jeu)</li>
-              <li>Le charisme (CHA en jeu)</li>
-            </ul>
+          </p>
+          <ul>
+            <li>La force (FOR en jeu)</li>
+            <li>La dextérité (DEX en jeu)</li>
+            <li>L'intelligence (INT en jeu)</li>
+            <li>Le charisme (CHA en jeu)</li>
+          </ul>
+
+          <p>
             Une de ces quatre caractéristiques sera la caractéristique dite "primaire" d'une entité. Cela sera représenté sur la fiche de l'entité par l'intitulé de la caractéristique en gras.
           </p>
           <p>
             Ces caractéristiques serviront au cours de l'aventure à déterminer l'issue d'une action. Par exemple :
-            <ul>
-              <li>Intimider une entité pourrait être plus simple pour un personnage possédant une force élevée</li>
-              <li>Scruter une pièce sans se faire repéré par de potentielles ennemis pourrait être plus simple pour un personnage possédant une dextérité élevée</li>
-              <li>Comprendre l'utilité d'un artefact pourrait être plus simple pour un personnage possédant une intelligence élevée</li>
-              <li>Diminuer l'hostilité d'une entité envers vous pourrait être plus simple pour un personnage possédant un charisme élevé</li>
-            </ul>
           </p>
+          <ul>
+            <li>Intimider une entité pourrait être plus simple pour un personnage possédant une force élevée</li>
+            <li>Scruter une pièce sans se faire repéré par de potentielles ennemis pourrait être plus simple pour un personnage possédant une dextérité élevée</li>
+            <li>Comprendre l'utilité d'un artefact pourrait être plus simple pour un personnage possédant une intelligence élevée</li>
+            <li>Diminuer l'hostilité d'une entité envers vous pourrait être plus simple pour un personnage possédant un charisme élevé</li>
+          </ul>
+
           <p>
             Ces caractéristiques seront aussi très importantes lors des combats (cf plus bas).
           </p>
@@ -51,24 +56,26 @@ const Rules = () => (
 
           <p>
             A ce stade, quatre classes sont jouables. La caractéristique primaire de la classe en question sera supérieure à la moyenne. La caractéristique primaire de chaque classe est :
-            <ul>
-              <li>Le guerrier : force</li>
-              <li>Le roublard : dextérité</li>
-              <li>Le magicien : intelligence</li>
-              <li>Le barde : charisme</li>
-            </ul>
           </p>
+          <ul>
+            <li>Le guerrier : force</li>
+            <li>Le roublard : dextérité</li>
+            <li>Le magicien : intelligence</li>
+            <li>Le barde : charisme</li>
+          </ul>
+
 
           <h2>Les jets</h2>
 
           <p>
             Un jet consiste à lancer un dé à 20 faces et y ajouter d'autres valeurs telles celle d'une caractéristique ou d'un bonus d'équipement. Trois types de jets peuvent avoir lieux au cours d'une partie :
-            <ul>
-              <li>Les jets d'action, lorsque le joueur doit effectuer un jet pour déterminer l'issue d'une action.</li>
-              <li>Les jets d'attaque, en combat lorsque le joueur tente d'attaquer un ennemi.</li>
-              <li>Les jets de parade, en combat lorsque le joueur tente de parer une attaque ennemie.</li>
-            </ul>
           </p>
+          <ul>
+            <li>Les jets d'action, lorsque le joueur doit effectuer un jet pour déterminer l'issue d'une action.</li>
+            <li>Les jets d'attaque, en combat lorsque le joueur tente d'attaquer un ennemi.</li>
+            <li>Les jets de parade, en combat lorsque le joueur tente de parer une attaque ennemie.</li>
+          </ul>
+
 
           <h2>Conditions de réussite d'un jet d'action</h2>
 
@@ -89,12 +96,13 @@ const Rules = () => (
           </p>
           <p>
             Il existe trois types d'équipements :
-            <ul>
-              <li>Les armes : bonus aux jets d'attaque</li>
-              <li>Les armures : bonus aux jets de défense</li>
-              <li>Les accessoires : bonus aux jets d'action</li>
-            </ul>
           </p>
+          <ul>
+            <li>Les armes : bonus aux jets d'attaque</li>
+            <li>Les armures : bonus aux jets de défense</li>
+            <li>Les accessoires : bonus aux jets d'action</li>
+          </ul>
+
 
           <h2>L'inventaire</h2>
 
@@ -106,39 +114,42 @@ const Rules = () => (
 
           <p>
             Cette interface sera décomposée en 2 parties :
-            <ul>
-              <li>A gauche :
-                <ul>
-                  <li>La contenu textuel du paragraphe en cours. Décrit la scène et les différentes actions qui vous sont proposées de faire.</li>
-                  <li>Des boutons. Permet d'envoyer une information au jeu :
-                    <ul>
-                      <li>Si un seul bouton : indique au jeu d'afficher le paragraphe suivant. Généralement "Continuer" sera inscrit à l'intérieur du bouton.</li>
-                      <li>Si plusieurs boutons : indique au jeu l'action que vous avez décidé d'exécuter. Un bref résumé de l'action en question sera inscrit à l'intérieur du bouton. Si l'issue d'une action est conditionnée par un jet de dé, la caractéristique utilisée pour déterminer l'issue sera indiquée entre guillemets.</li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>A droite :
-                <ul>
-                  <li>Une icone permettant d'accéder à un résumé des règles.</li>
-                  <li>Les caractéristiques du personnage joué. Sa caractéristique primaire est affichée en gras.</li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
-              </li>
-            </ul>
           </p>
+          <ul>
+            <li>A gauche :
+                <ul>
+                <li>La contenu textuel du paragraphe en cours. Décrit la scène et les différentes actions qui vous sont proposées de faire.</li>
+                <li>Des boutons. Permet d'envoyer une information au jeu :
+                    <ul>
+                    <li>Si un seul bouton : indique au jeu d'afficher le paragraphe suivant. Généralement "Continuer" sera inscrit à l'intérieur du bouton.</li>
+                    <li>Si plusieurs boutons : indique au jeu l'action que vous avez décidé d'exécuter. Un bref résumé de l'action en question sera inscrit à l'intérieur du bouton. Si l'issue d'une action est conditionnée par un jet de dé, la caractéristique utilisée pour déterminer l'issue sera indiquée entre guillemets.</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>A droite :
+                <ul>
+                <li>Une icone permettant d'accéder à un résumé des règles.</li>
+                <li>Les caractéristiques du personnage joué. Sa caractéristique primaire est affichée en gras.</li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </li>
+          </ul>
+
 
           <h2>Interface de jeu durant les combats</h2>
           {/* à compléter */}
+          <div className="hr" />
         </div>
-        </div>
-        <div className="rules__page rules__page__right shadow">
-          
-            
+        <div className="rules__page  shadow">
+          <div className="rules__page__right__fake_image">
+            <FaImages size={400} />
+          </div>
         </div>
       </div>
+    </div>
   </section>
 );
 
