1cf766f4bf8d7e175528cf93b220a67bddbed72d
guillaume806 <guillaumebodart@outlook.fr>
2021-11-08 12:19:13 +0100

    feature-IngameFight

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -21,6 +21,7 @@ import Signin from 'src/components/Signin';
 import Signup from 'src/components/Signup';
 import SelectCharacter from 'src/components/SelectCharacter';
 import Ingame from 'src/components/Ingame';
+import IngameFight from 'src/components/IngameFight';
 import Error404 from 'src/components/Error404';
 import DiceRoller from 'src/components/DiceRoller';
 
@@ -70,6 +71,9 @@ const App = () => {
         </Route>
         <Route exact path="/jouer">
           <Ingame />
+        </Route>
+        <Route exact path="/combat">
+          <IngameFight />
         </Route>
         {
           isLogged && (
