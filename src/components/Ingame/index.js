430e33a05b52c8baa50b2cd61c4923247d650094
Maxence-TASSERY <maxence.tassery@gmail.com>
2021-11-08 15:27:13 +0100

    suppression de lignes inutiles

--- src/components/Ingame/index.js
+++ src/components/Ingame/index.js
@@ -4,50 +4,15 @@ import InGameLeft from './IngameLeft/InGameLeft';
 
 import './ingame.scss';
 
-const Ingame = () => {
-  const {
-    characterMaxHp,
-    characterCurrentHp,
-    characterStrength,
-    characterDexterity,
-    characterIntelligence,
-    characterCharism,
-    weaponName,
-    weaponBonus,
-    armorName,
-    armorBonus,
-    accessoryName,
-    accessoryBonus,
-    characterPicture,
-    inventoryName,
-    characterPrimaryCharacteristic,
-  } = useSelector((state) => ({
-    characterMaxHp: state.game.characterMaxHp,
-    characterCurrentHp: state.game.characterCurrentHp,
-    characterStrength: state.game.characterStrength,
-    characterDexterity: state.game.characterDexterity,
-    characterIntelligence: state.game.characterIntelligence,
-    characterCharism: state.game.characterCharism,
-    weaponName: state.game.weaponName,
-    weaponBonus: state.game.weaponBonus,
-    armorName: state.game.armorName,
-    armorBonus: state.game.armorBonus,
-    accessoryName: state.game.accessoryName,
-    accessoryBonus: state.game.accessoryBonus,
-    characterPicture: state.game.characterPicture,
-    inventoryName: state.game.inventoryName,
-    characterPrimaryCharacteristic: state.game.characterPrimaryCharacteristic,
-  }));
-  return (
-    <section className="ingame">
-      <div className="ingame__border shadow">
-        <div className="ingame__pages shadow">
-          <InGameLeft />
-          <InGameRight />
-        </div>
+const Ingame = () => (
+  <section className="ingame">
+    <div className="ingame__border shadow">
+      <div className="ingame__pages shadow">
+        <InGameLeft />
+        <InGameRight />
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default Ingame;
