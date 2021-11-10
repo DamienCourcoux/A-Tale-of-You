05ee7ef8b32c4048adbfad3fde13a5f967582acd
DamienCourcoux <damsmailspro@gmail.com>
2021-11-10 09:31:33 +0100

    refacto rules

---
+++ src/components/Rules/RulesRight/rulesRight.js
@@ -0,0 +1,14 @@
+// == Import
+import { FaImages } from 'react-icons/fa';
+
+import './rulesRight.scss';
+
+// == Composant
+const RulesRight = () => (
+  <div className="rulesRight shadow">
+    <FaImages size={400} />
+  </div>
+);
+
+// == Export
+export default RulesRight;
