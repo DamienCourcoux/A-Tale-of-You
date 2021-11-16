4c2da3cbaf6255e8984817be487b7bf753cb850d
DamienCourcoux <damsmailspro@gmail.com>
2021-11-16 11:24:34 +0100

    ajout de l'image page de règles

--- src/components/Rules/RulesRight/index.js
+++ src/components/Rules/RulesRight/index.js
@@ -1,14 +1,18 @@
 // == Import
-import { FaImages } from 'react-icons/fa';
+import ImgRegles from 'src/assets/regles.png';
 
 import './style.scss';
 
 // == Composant
 const RulesRight = () => (
-  <div className="page page_right shadow page--remove_mobile">
-    <div className="page_img">
-      <FaImages size={400} />
-    </div>
+  <div className="page_center page page_right shadow page--remove_mobile">
+    <img
+      className="page--remove_mobile"
+      title="Img de Dženis Hasanica sur Pexels"
+      src={ImgRegles}
+      alt="Illustration des règles
+      (crédit: Img de Dženis Hasanica sur Pexels)"
+    />
   </div>
 );
 
