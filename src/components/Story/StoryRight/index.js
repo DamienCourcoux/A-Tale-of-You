ebe8b79430b6abcde4b9d753cb4a0d59c49a42d7
DamienCourcoux <damsmailspro@gmail.com>
2021-11-16 11:08:19 +0100

    ajout d'un image pour la page histoire

--- src/components/Story/StoryRight/index.js
+++ src/components/Story/StoryRight/index.js
@@ -1,9 +1,23 @@
 // == Import
+import ImgHistoire from 'src/assets/histoire.jpg';
+
 import './style.scss';
 
 // == Composant
 const StoryRight = () => (
-  <div className="page page_right shadow page--remove_mobile" />
+  <div className="page_center page page_right shadow page--remove_mobile">
+    <img
+      className="page--remove_mobile"
+      title="Dungeon of the Polignac Castle,
+      Eugène Isabey, Metropolitan Museum of Art,
+      New York, NY"
+      src={ImgHistoire}
+      alt="illustration de l'histoire
+      (crédit: Dungeon of the Polignac Castle,
+      Eugène Isabey, Metropolitan Museum of Art,
+      New York, NY)"
+    />
+  </div>
 );
 
 // == Export
